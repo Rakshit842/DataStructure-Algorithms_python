2998cf8dc7d0b91562bf7d4adfc1d3cbd90a5207
rakshit kamboj <rakshitkamboj2@gmail.com>
2026-01-30 14:04:32 +0530

    DAY 3

---
+++ DAY 3.py
@@ -0,0 +1,22 @@
+# Finding first maximum number
+arr = [12, 7, 8, 9, 0]
+maxno = arr[0]
+for i in range(len(arr)):
+    if arr[i] > maxno:
+        maxno=arr[i]
+print("maximum number is", maxno)
+
+# Finding second maximum number
+import math
+arr1 = [12, 7, 8, 9, 0]
+firstmax = -math.inf
+secondmax = -math.inf
+
+for curValue in arr1:
+    if curValue > firstmax:
+        secondmax = firstmax
+        firstmax = curValue
+
+    elif curValue > secondmax:
+        secondmax = curValue
+print("second maximum number is", secondmax)
